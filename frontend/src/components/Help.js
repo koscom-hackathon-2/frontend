2c34e4708ad95c413bc61db9b179485f6e946abc
sonjisu <donediddo@daum.net>
2024-08-01 17:34:45 +0900

    (no-issue) help href 추가

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -11,17 +11,20 @@ function Help() {
                     <span className="title">HELP</span>
 
                     <div className="container">
-                        <a href="https://www.koscom.co.kr/portal/main/contents.do?menuNo=200612" className="card">
+                        <a href="https://datamall.koscom.co.kr/kor/main/contents.do?menuNo=200082" target="_blank"
+                           className="card">
                             <div className="title">GUIDE</div>
                             <img src={guide} className="icon"></img>
                             <div className="subscript">기초 가이드를 확인하세요.</div>
                         </a>
-                        <a href="https://www.koscom.co.kr/portal/main/contents.do?menuNo=200612" className="card">
+                        <a href="https://datamall.koscom.co.kr/kor/bbs/B0000003/list.do?menuNo=200083" target="_blank"
+                           className="card">
                             <div className="title">F&Q</div>
                             <img src={fnq} className="icon"></img>
                             <div className="subscript">자주 묻는 질문을 모아놨어요.</div>
                         </a>
-                        <a href="https://www.koscom.co.kr/portal/main/contents.do?menuNo=200612" className="card">
+                        <a href="https://datamall.koscom.co.kr/kor/bbs/B0000001/list.do?menuNo=200081" target="_blank"
+                           className="card">
                             <div className="title">NOTICE</div>
                             <img src={notice} className="icon"></img>
                             <div className="subscript">공지사항을 확인하세요.</div>
